dd2a716a2b53f8badab5ac4234bf1f05fd67d29f
Данил <abanin-02@mail.ru>
2021-09-18 23:58:20 +0400

    added adaptive layout

--- src/components/MainPage/Slider/slider.tsx
+++ src/components/MainPage/Slider/slider.tsx
@@ -1,17 +1,26 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import arrow from "./images/arrow.svg"
 import "./slider.scss"
 import {SliderData} from "./slidersData";
 import Slide from "./slide";
 const Slider:React.FC = () => {
     const [position, setPosition] = useState<number>(0)
+    const [arrayPositions, setArrayPositions] = useState<number[] | []>([])
+
+    useEffect(() => {
+        let array = []
+        for(let i = 0; i < SliderData.length; i++){
+            array.push(i)
+        }
+        setArrayPositions(array)
+    }, [])
 
     const goToNextSlide = () => {
-        const newPosition = position === 3 ? 0 : position + 1;
+        const newPosition = position === SliderData.length - 1 ? 0 : position + 1;
         setPosition(newPosition)
     };
     const goToPrevSlide = () => {
-        const newPosition = position === 0 ? 3 : position - 1;
+        const newPosition = position === 0 ? SliderData.length - 1 : position - 1;
         setPosition(newPosition)
     };
 
@@ -32,6 +41,13 @@ const Slider:React.FC = () => {
             >
                 {SliderData.map(sliderInfo =>
                     <Slide information={sliderInfo}/>
+                )}
+            </div>
+            <div className={"blockNumberOfSlides"}>
+                {arrayPositions.map(index =>
+                    <div className={`${index === position ? "activeSlideNumber" : ""}`}>
+
+                    </div>
                 )}
             </div>
         </div>
