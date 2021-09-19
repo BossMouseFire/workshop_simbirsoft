1eb14e1363e37ac7892247b4ec82e2f959f04950
Данил <abanin-02@mail.ru>
2021-09-19 23:27:15 +0400

    fixed size background slide

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from "react"
-import {Switch, Route, Redirect, BrowserRouter} from "react-router-dom";
+import {Switch, Route, Redirect} from "react-router-dom";
 import MainPage from "./components/MainPage/mainPage";
 
 const App:React.FC = () => {
     return(
-        <BrowserRouter>
             <Switch>
                 <Route exact path={"/"} component={MainPage}/>
                 <Redirect to={"/"}/>
             </Switch>
-        </BrowserRouter>
     )
 }
 export default App;
